140e1292762ccdda1974f9db75fb4085bbca76a4
Nimesh Gunasekara <admin@statsis.com>
2016-09-12 23:28:13 +0530

    bootstrap frotend work init

--- src/StatsisWeb/gulpfile.js
+++ src/StatsisWeb/gulpfile.js
@@ -26,11 +26,13 @@ gulp.task('clean', function (cb) {
     return rimraf("./wwwroot/lib", cb);
 });
 
-gulp.task("copy:lib", function () {
+gulp.task("copy:lib", ["clean"], function () {
     gulp.src(paths.angular2).pipe(gulp.dest("./wwwroot/lib/angular2/"));
     gulp.src([paths.node + "rxjs/**/*.js"]).pipe(gulp.dest("./wwwroot/lib/rxjs/"));
     gulp.src([paths.node + "systemjs/dist/system.src.js"]).pipe(gulp.dest("./wwwroot/lib/"));
     gulp.src([paths.node + "core-js/client/shim.min.js"]).pipe(gulp.dest("./wwwroot/lib/"));
     gulp.src([paths.node + "zone.js/dist/zone.js"]).pipe(gulp.dest("./wwwroot/lib/"));
     gulp.src([paths.node + "reflect-metadata/Reflect.js"]).pipe(gulp.dest("./wwwroot/lib/"));
+    gulp.src([paths.node + "bootstrap/dist/**/*"]).pipe(gulp.dest("./wwwroot/lib/bootstrap/"));
+    gulp.src([paths.node + "jquery/dist/jquery.js"]).pipe(gulp.dest("./wwwroot/lib/"));
 });
