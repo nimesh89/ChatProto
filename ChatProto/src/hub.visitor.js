cd601ad3c00d793350100a4d8104fa24cd64594c
Nimesh Gunasekara <nimesh@bizchatbox.com>
2017-09-08 11:54:55 +0530

    visitor chat initial commit

--- ChatProto/src/hub.visitor.js
+++ ChatProto/src/hub.visitor.js
@@ -1,29 +1,65 @@
 ﻿import ngmodule from "./app"
 
-var chatHubProxy = $.connection.chatHub;
+ngmodule.config(["$stateProvider", "$urlServiceProvider", function ($stateProvider, $urlServiceProvider) {
+    $stateProvider.state('home', {
+        url: '/vi',
+        templateUrl: 'startbutton.html',
+        controller: 'visitorCtrl'
+    });
 
-chatHubProxy.client.recievedMessage = function (vid, sourceid, sourceName, sourcetime, messagetype, message) {
+    $stateProvider.state('home.chat', {
+        url: '/chat',
+        templateUrl: 'inchat.html',
+        controller: 'visitorChatCtrl'
+    });
 
+    $urlServiceProvider.rules.otherwise({ state: 'home' });
+}]);
+
+class VisitorChat {
+
+    constructor($rootScope, $q, $timeout) {
+        this.$rootScope = $rootScope;
+        this.$q = $q;
+        this.$timeout = $timeout;
+        var chatHubProxy = $.connection.chatHub;
+
+        chatHubProxy.client.recievedMessage = function (vid, sourceid, sourceName, sourcetime, messagetype, message) {
+
+        }
+
+        chatHubProxy.client.opJoined = function (vid, opid, opname, time) {
+
+        }
+
+        chatHubProxy.client.opWentOffline = function (vid, opid, opname, time) {
+
+        }
+
+        chatHubProxy.client.opCameOnline = function (vid, opid, opname, time) {
+
+        }
+
+        chatHubProxy.client.chatClosed = function (vid, opid, opname, time) {
+
+        }
+
+        $.connection.hub.start().done(function () {
+
+        });
+    }
+
+    start(name) {
+       
+    }
+
+    stop() {
+       
+    }
 }
 
-chatHubProxy.client.opJoined = function (vid, opid, opname, time) {
+ngmodule.factory('operatoChatService', ["$rootScope", "$q", "$timeout", function ($rootScope, $q, $timeout) {
+    return new VisitorChat($rootScope, $q, $timeout);
+}]);
 
-}
-
-chatHubProxy.client.opWentOffline = function (vid, opid, opname, time) {
-
-}
-
-chatHubProxy.client.opCameOnline = function (vid, opid, opname, time) {
-
-}
-
-chatHubProxy.client.chatClosed = function (vid, opid, opname, time) {
-
-}
-
-$.connection.hub.start().done(function () {
-
-});
-
-export default chatHubProxy;
+export default VisitorChat;
--- ChatProto/src/visitor.js
+++ ChatProto/src/visitor.js
@@ -1,2 +1,25 @@
 ﻿import ngmodule from "./app"
 import hub from "./hub.visitor"
+
+ngmodule.controller("visitorCtrl", ["$scope", "visitorChatService", "$state", function ($scope, visitorChatService, $state) {
+    $scope.vi = {};
+    $scope.startHub = function () {
+        visitorChatService.start($scope.vi.viname)
+            .then(function () {
+                $state.go("home.chat", { name: $scope.vi.viname })
+                $scope.vi.ChatService = true;;
+            });
+    }
+
+    $scope.stopHub = function () {
+        visitorChatService.stop()
+            .then(function () {
+                $scope.op = {};
+                $state.go("home");
+            });
+    }
+}]);
+
+ngmodule.controller("visitorChatCtrl", ["$scope", "operatoChatService", "$state", function ($scope, operatoChatService, $state) {
+
+}]);
